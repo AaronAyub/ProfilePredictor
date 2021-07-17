5421a30da10ba9188a304c5a19335a03a07cde82
Aaron Ayub <55106255+AaronAyub@users.noreply.github.com>
2021-07-17 10:44:56 -0400

    Created "About" Dialog to further explain website use.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,24 @@
-import { AppBar, Toolbar, Typography, Button, Menu, MenuItem, Link } from '@material-ui/core'
+import { Container, Divider, AppBar, Toolbar, Typography, Button, Menu, MenuItem, Link, Dialog, DialogTitle, DialogActions } from '@material-ui/core'
 import React, { useState } from 'react'
 
 function Header (): JSX.Element {
     const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null)
+    const [open, setOpen] = useState<boolean>(false)
 
-    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    // Functions used to open/close the API menu
+    const handleApiOpen = (event: React.MouseEvent<HTMLButtonElement>) => {
         setAnchorEl(event.currentTarget)
     }
-    const handleClose = () => {
+    const handleApiClose = () => {
         setAnchorEl(null)
+    }
+
+    // Functions used to open/close the About dialog
+    const handleAboutOpen = () => {
+        setOpen(true)
+    }
+    const handleAboutClose = () => {
+        setOpen(false)
     }
 
     return (
@@ -17,7 +27,10 @@ function Header (): JSX.Element {
             <Toolbar className="toolbar" variant="dense">
                 <Typography variant="h6">Profile Predictor</Typography>
                 <div>
-                    <Button color="inherit" aria-controls="apis" aria-haspopup="true" onClick={handleClick}>
+                    <Button color="inherit" aria-controls="about" aria-haspopup="true" onClick={handleAboutOpen}>
+                        About
+                    </Button>
+                    <Button color="inherit" aria-controls="apis" aria-haspopup="true" onClick={handleApiOpen}>
                         APIs
                     </Button>
                     <Button href="https://github.com/AaronAyub/ProfilePredictor" color="inherit">GITHUB</Button>
@@ -26,23 +39,55 @@ function Header (): JSX.Element {
         </AppBar>
         <Toolbar variant="dense"></Toolbar> {/* This second toolbar is used to take up space and not hide elements of the page. */}
         
+        {/* A dialog popup with some information on the project. */}
+        <Dialog onClose={handleAboutClose} aria-labelledby="aboutTitle" open={open}>
+            <DialogTitle id="aboutTitle">About</DialogTitle>
+            <Container>
+                <p>Profile Predictor is a web-based interface used to predict certain attributes associated with names.
+                These attributes include age, gender, and nationality.</p>
+                <p>Upon entering a name, the query will return a predicted age, gender, probability of that name being associated with the given gender, and the three most likely countries this name belongs to.</p>
+                <p>Predictions are performed by querying three APIs,&nbsp;
+                <Link href="https://agify.io/">
+                    agify.io
+                </Link>,&nbsp;
+                <Link href="https://genderize.io/">
+                    genderize.io
+                </Link>, and&nbsp;
+                <Link href="https://nationalize.io/">
+                    nationalize.io
+                </Link>.
+                    These APIs are provided by Demographix ApS.</p>
+            </Container>
+            <Divider/>
+            <DialogTitle id="disclaimer">Disclaimer</DialogTitle>
+            <Container>
+                This website and the developers are not associated with Demografix ApS in any way. Demografix ApS provides public APIs used by this website to make predictions, as noted above. This software does not provide any guarantees regarding the accuracy or completeness of returned predictions.
+                <DialogActions>
+                <Button onClick={handleAboutClose} color="primary">
+                    Okay
+                </Button>
+            </DialogActions>
+            </Container>
+            
+        </Dialog>
+
         {/* This menu contains links to all apis used. */}
         <Menu id="apis"
         anchorEl={anchorEl}
         keepMounted
         open={Boolean(anchorEl)}
-        onClose={handleClose}>
-            <MenuItem onClick={handleClose}>
+        onClose={handleApiClose}>
+            <MenuItem onClick={handleApiClose}>
                 <Link href="https://agify.io/">
                     agify.io
                 </Link>
             </MenuItem>
-            <MenuItem onClick={handleClose}>
+            <MenuItem onClick={handleApiClose}>
                 <Link href="https://genderize.io/">
                     genderize.io
                 </Link>
             </MenuItem>
-            <MenuItem onClick={handleClose}>
+            <MenuItem onClick={handleApiClose}>
                 <Link href="https://nationalize.io/">
                     nationalize.io
                 </Link>
